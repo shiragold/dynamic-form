4efdee3901ee59553a9383e429580899644867b3
shiragold <shira14@gmail.com>
2026-02-18 06:04:10 +0200

    Refactor App structure by removing router integration and replacing RouterView with HomeView. Update tests to reflect changes in the app's rendering logic.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router'
+import HomeView from '@/views/HomeView.vue'
 </script>
 
 <template>
-  <RouterView />
+  <HomeView />
 </template>
--- src/__tests__/App.spec.ts
+++ src/__tests__/App.spec.ts
@@ -1,23 +1,13 @@
 import { describe, it, expect } from 'vitest'
 import { mount } from '@vue/test-utils'
-import { createRouter, createWebHistory } from 'vue-router'
 import { createPinia } from 'pinia'
 import App from '../App.vue'
-import HomeView from '../views/HomeView.vue'
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes: [{ path: '/', component: HomeView }],
-})
 
 describe('App', () => {
-  it('renders properly', async () => {
-    router.push('/')
-    await router.isReady()
-
+  it('renders properly', () => {
     const wrapper = mount(App, {
       global: {
-        plugins: [router, createPinia()],
+        plugins: [createPinia()],
       },
     })
 
